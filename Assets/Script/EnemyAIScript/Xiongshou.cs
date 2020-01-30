fe1939ed054ee9dfdc455cb15bd2f1193596df82
Jay li <44041337+Jayli22@users.noreply.github.com>
2020-01-30 12:36:25 -0500

    Fungus update/Xiongshou Added

---
+++ Assets/Script/EnemyAIScript/Xiongshou.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xiongshou : MeleeEnemy
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        if (isAlive)
+        {
+            if (!isStiffness)
+            {
+                AttackDetection(0.5f);
+                AlarmRadiusDetection();
+            }
+        }
+        else
+        {
+            canMove = false;
+            hitable = false;
+            DeathComing();
+        }
+    }
+
+
+
+
+    public override void TakeDamage(int damage)
+    {
+        if (hitable)
+        {
+            base.TakeDamage(damage);
+            //DoStiffness();
+            Debug.Log("我受到了攻击");
+        }
+    }
+
+
+}
